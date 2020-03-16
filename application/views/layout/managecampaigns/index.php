070ea72d76d8dbd3c0374ef4b20cce02846966c3
camkh <socheatnews@hotmail.com>
2020-03-16 09:58:40 +0700

    fixed

--- application/views/layout/managecampaigns/index.php
+++ application/views/layout/managecampaigns/index.php
@@ -313,15 +313,17 @@ function parse_query_string(query) {
         $uniq_id = substr($str, 0, 9);
         //$link = $glink . '?s=' . $uniq_id;
         $link = $glink;
-        $UserTable = new Mod_general ();
-    	$getBrowser = $UserTable->getBrowser()['name'];
-    	if($getBrowser=='Mozilla Firefox'){
-	        $parse = parse_url($glink);
-	        if (in_array(@$parse['host'], $siteUrl)) {
-	        	$pid = $value->{Tbl_posts::id};
-	        	echo '<script language="javascript" type="text/javascript">window.setTimeout( function(){window.location = "'.base_url().'managecampaigns/yturl?pid='.$pid.'&action=postblog&autopost=1";},0 );</script>';
-	            exit();
-	        } 
+        if(!empty($this->session->userdata('post_only'))) {
+	        $UserTable = new Mod_general ();
+	    	$getBrowser = $UserTable->getBrowser()['name'];
+	    	if($getBrowser=='Mozilla Firefox'){
+		        $parse = parse_url($glink);
+		        if (in_array(@$parse['host'], $siteUrl)) {
+		        	$pid = $value->{Tbl_posts::id};
+		        	echo '<script language="javascript" type="text/javascript">window.setTimeout( function(){window.location = "'.base_url().'managecampaigns/yturl?pid='.$pid.'&action=postblog&autopost=1";},0 );</script>';
+		            exit();
+		        } 
+	    	}
     	}
      ?>
                                     <tr>
