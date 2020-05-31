31fee74ecbb5828e6c66613694e6aa3b526c29b0
camkh <socheatnews@hotmail.com>
2020-05-31 12:38:59 +0700

    d

--- application/controllers/managecampaigns.php
+++ application/controllers/managecampaigns.php
@@ -2602,7 +2602,8 @@ class Managecampaigns extends CI_Controller {
                 }
             }
             if(!empty($post_next)) {
-                echo '<script>window.setTimeout( function(){window.location = "'.base_url().'managecampaigns/yturl?pid='.$post_next.'&action=postblog&autopost=1";},‭300‬ );</script>';
+
+                echo '<script>window.setTimeout( function(){window.location = "'.base_url().'managecampaigns/yturl?pid='.$post_next.'&action=postblog&autopost=1";}, 30 );</script>';
                         exit();
                 // if($this->session->userdata('p_id') != $post_next) {
                 //     if(empty($this->session->userdata('p_time'))) {
