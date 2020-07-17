03d16a8f8353a90d4fc10600fb11ef16bc73b247
camkh <socheatnews@hotmail.com>
2020-07-17 18:52:55 +0700

    add site

--- application/controllers/Getcontent.php
+++ application/controllers/Getcontent.php
@@ -3701,6 +3701,36 @@ $link =  $desc->find('a', 0)->href;
                 $obj->site = 'site';
                 return $obj;
                 break;
+            case 'plus-newsth.com':
+                //$obj->title = @$html->find ( '.post .entry-title', 0 )->plaintext;
+                //$obj->thumb = @$html->find ( '.content-img-all .img-hi-news img', 0 )->src;
+                foreach($html->find('.post-share') as $item) {
+                    $item->outertext = '';
+                }
+                $html->save();
+                $content = @$html->find ( '.single-post-content', 0 );
+                $content = preg_replace('/<center\b[^>]*>(.*?)<\/center>/is', "", $content);
+                $content = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $content);
+                $content = preg_replace('/<ins\b[^>]*>(.*?)<\/ins>/is', '<div class="setAds"></div>', $content);
+                $content = preg_replace("/<a(.*?)>/", "<a$1 target=\"_blank\">", $content);
+                $content = preg_replace( '/(<[^>]+) srcset=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-lazy-srcset=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-lazy-sizes=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-lazy-src=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-recalc-dims=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-large-file=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-medium-file=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-image-meta=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-image-description class=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-orig-file=".*?"/i', "$1", $content );
+                $content = preg_replace( '/(<[^>]+) data-permalink=".*?"/i', "$1", $content );
+                $content = str_replace('--Advertisement--', '', $content);
+                $obj->vid = '';
+                $obj->conent = $content;
+                $obj->fromsite = $parse['host'];
+                $obj->site = 'site';
+                return $obj;
+                break;
             default:
                 if (preg_match ( '/blogspot/', $url ) && empty($oldurl)) {
                     $obj->thumb = $this->mod_general->resize_image($obj->thumb,0);
